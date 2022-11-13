1838653066e2d2a16432a72fafd3e6b58af6a285
Roy <lxysac@foxmail.com>
2022-11-13 18:29:56 +0800

    重构项目结构

---
+++ Application/DTO/SearchParams.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTO;
+
+public class SeachParams
+{
+    /// <summary>
+    /// 页码
+    /// </summary>
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int Page { get; set; } = 1;
+
+    /// <summary>
+    /// 每页数据量
+    /// </summary>
+    [Required]
+    public int PageSize { get; set; } = 20;
+}
