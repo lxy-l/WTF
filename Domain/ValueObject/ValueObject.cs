884380d48297bb1c12a725ac3afcdfbb52cd9304
Roy <lxysac@foxmail.com>
2022-11-14 11:25:31 +0800

    添加领域模型单元测试

--- Domain/ValueObject/ValueObject.cs
+++ Domain/ValueObject/ValueObject.cs
@@ -5,40 +5,4 @@
 /// </summary>
 public abstract record ValueObject<T> where T : ValueObject<T>
 {
-    //public override bool Equals(object? obj)
-    //{
-    //    var valueObject = obj as T;
-    //    return !ReferenceEquals(valueObject, null) && EqualsCore(valueObject);
-    //}
-
-    //protected abstract bool EqualsCore(T other);
-
-    //public override int GetHashCode()
-    //{
-    //    return GetHashCodeCore();
-    //}
-
-    //protected abstract int GetHashCodeCore();
-
-    //public static bool operator ==(ValueObject<T> value1, ValueObject<T> value2)
-    //{
-    //    if (ReferenceEquals(value1, null) && ReferenceEquals(value2, null))
-    //        return true;
-
-    //    if (ReferenceEquals(value1, null) || ReferenceEquals(value2, null))
-    //        return false;
-
-    //    return value1.Equals(value2);
-    //}
-
-    //public static bool operator !=(ValueObject<T> value1, ValueObject<T> value2)
-    //{
-    //    return !(value1 == value2);
-    //}
-
-    //public virtual T Clone()
-    //{
-    //    return (T)MemberwiseClone();
-    //}
-
 }
---
+++ DomainTests/Entities/UserTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Entities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.ValueObject;
+
+namespace Domain.Entities.Tests
+{
+    /// <summary>
+    /// 模型单元测试
+    /// </summary>
+    [TestClass()]
+    public class UserTests
+    {
+        [TestMethod()]
+        public void UserTest()
+        {
+            User user = 
+                new User("1", DateTimeOffset.Now, 
+                new Address("A", "B", "C", "D"),1);
+            User user2 =
+                new User("2", DateTimeOffset.Now,
+                new Address("A", "B", "C", "D"),2);
+
+            User user3 =
+                new User("3", DateTimeOffset.Now,
+                new Address("A", "B", "C", "E"), 3);
+
+            Assert.IsFalse(user.Equals(user2));
+            Assert.IsTrue(user2.Address.Equals(user.Address));
+           
+
+            Assert.IsFalse(user==user2);
+            Assert.IsTrue(user.Address==user2.Address);
+            Assert.IsTrue(user2 != user);
+
+            Assert.IsFalse(user3.Address==user2.Address);
+            Assert.IsFalse(user3.Address.Equals(user2.Address));
+            Assert.IsTrue(user3.Address != user2.Address);
+        }
+
+        [TestMethod()]
+        public void UserTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
