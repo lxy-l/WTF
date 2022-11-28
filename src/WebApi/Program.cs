12b9eba2a3530edd177ac4ceef7a24d714241cc5
Roy <lxysac@foxmail.com>
2022-11-28 10:28:45 +0800

    XML文档

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -139,10 +139,16 @@ builder.Services.AddSwaggerGen(config =>
     config.OrderActionsBy(o => o.RelativePath);
     var xmlPath = Path.Combine(basePath, "WebApi.xml");
     config.IncludeXmlComments(xmlPath, true);
+    var xmlPath1 = Path.Combine(basePath, "WebApi.Core.xml");
+    config.IncludeXmlComments(xmlPath1, true);
     var xmlModelPath = Path.Combine(basePath, "Domain.xml");
     config.IncludeXmlComments(xmlModelPath);
+    var xmlModelPath1 = Path.Combine(basePath, "Domain.Core.xml");
+    config.IncludeXmlComments(xmlModelPath1);
     var xmlApplicationPath = Path.Combine(basePath, "Application.xml");
     config.IncludeXmlComments(xmlApplicationPath);
+    var xmlApplicationPath1 = Path.Combine(basePath, "Application.Core.xml");
+    config.IncludeXmlComments(xmlApplicationPath1);
     config.OperationFilter<AddResponseHeadersFilter>();
     config.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
     config.OperationFilter<SecurityRequirementsOperationFilter>();
