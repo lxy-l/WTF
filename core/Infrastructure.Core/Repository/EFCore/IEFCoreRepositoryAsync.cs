cdd78a0091b52a095e51755513707c7ea380beed
Roy <lxysac@foxmail.com>
2022-11-29 14:36:51 +0800

    EFCore多表联查

--- core/Infrastructure.Core/Repository/EFCore/IEFCoreRepositoryAsync.cs
+++ core/Infrastructure.Core/Repository/EFCore/IEFCoreRepositoryAsync.cs
@@ -35,6 +35,6 @@ namespace Infrastructure.Core.Repository
         /// <param name="sort">排序条件</param>
         /// <param name="include">子表</param>
         /// <returns></returns>
-        Task<IQueryable<TEntity>> GetDynamicQueryAsync(string? filter=null,string? sort=null, string? include = null);
+        Task<IQueryable<TEntity>> GetDynamicQueryAsync(string? filter=null,string? sort=null, string[]? include = null);
     }
 }
