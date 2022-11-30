e5fa4845dc22dfddf6910346e445251b65d0687e
Roy <lxysac@foxmail.com>
2022-11-30 16:19:49 +0800

    Consul网关配置

---
+++ test/Infrastructure.CoreTests/Extend/GuidExtendTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Infrastructure.Core.Extend.Tests
+{
+    [TestClass()]
+    public class GuidExtendTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            var guid1= GuidExtend.Create();
+            Assert.IsNotNull(guid1);
+        }
+    }
+}
