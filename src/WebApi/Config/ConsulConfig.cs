aa52b7e8f70ca11e790549f9630d73b6a9d66e9c
Roy <lxysac@foxmail.com>
2022-11-30 17:32:44 +0800

    .

--- src/WebApi/Config/ConsulConfig.cs
+++ src/WebApi/Config/ConsulConfig.cs
@@ -1,55 +1,57 @@
 ﻿using Consul;
 using Consul.AspNetCore;
 
-namespace WebApi.Config
+namespace WebApi.Config;
+
+/// <summary>
+/// Consul网关配置
+/// </summary>
+public static class ConsulConfig
 {
-    public static class ConsulConfig
+    public static IServiceCollection AddConsulConfig(this IServiceCollection Services, IConfiguration Configuration)
     {
-        public static IServiceCollection AddConsulConfig(this IServiceCollection Services, IConfiguration Configuration)
+
+        var config = Configuration.GetSection("Consul");
+        Services.AddConsul(options =>
         {
-
-            var config = Configuration.GetSection("Consul");
-            Services.AddConsul(options =>
+            options.Address = new Uri(config["Address"]);
+            options.WaitTime = TimeSpan.FromSeconds(
+                config["WaitTime"] is not null
+                ? double.Parse(config["WaitTime"])
+                : 30);
+        });
+        var service = config.GetSection("Service");
+        Services.AddConsulServiceRegistration(options =>
+        {
+            options.ID = service["ID"];
+            options.Name = service["Name"];
+            options.Port = service["Port"] is not null
+                ? int.Parse(service["Port"])
+                : throw new Exception("未服务配置端口！");
+            options.Address = service["Address"];
+            options.Check = new AgentServiceCheck()
             {
-                options.Address = new Uri(config["Address"]);
-                options.WaitTime = TimeSpan.FromSeconds(
-                    config["WaitTime"] is not null
-                    ? double.Parse(config["WaitTime"])
-                    : 30);
-            });
-            var service = config.GetSection("Service");
-            Services.AddConsulServiceRegistration(options =>
-            {
-                options.ID = service["ID"];
-                options.Name = service["Name"];
-                options.Port = service["Port"] is not null
-                    ? int.Parse(service["Port"])
-                    : throw new Exception("未服务配置端口！");
-                options.Address = service["Address"];
-                options.Check = new AgentServiceCheck()
-                {
-                    DeregisterCriticalServiceAfter = service["DeregisterCriticalServiceAfter"] is not null
-                        ? TimeSpan.FromSeconds(
-                            double.Parse(
-                                service["DeregisterCriticalServiceAfter"]))
-                        : TimeSpan.FromSeconds(20),
+                DeregisterCriticalServiceAfter = service["DeregisterCriticalServiceAfter"] is not null
+                    ? TimeSpan.FromSeconds(
+                        double.Parse(
+                            service["DeregisterCriticalServiceAfter"]))
+                    : TimeSpan.FromSeconds(20),
 
-                    HTTP = service["HTTP"],
+                HTTP = service["HTTP"],
 
-                    Timeout = service["Timeout"] is not null
-                        ? TimeSpan.FromSeconds(
-                            double.Parse(
-                                service["Timeout"]))
-                        : TimeSpan.FromSeconds(20),
+                Timeout = service["Timeout"] is not null
+                    ? TimeSpan.FromSeconds(
+                        double.Parse(
+                            service["Timeout"]))
+                    : TimeSpan.FromSeconds(20),
 
-                    Interval = service["Interval"] is not null
-                        ? TimeSpan.FromSeconds(
-                            double.Parse(
-                                service["Interval"]))
-                        : TimeSpan.FromSeconds(20)
-                };
-            });
-            return Services;
-        }
+                Interval = service["Interval"] is not null
+                    ? TimeSpan.FromSeconds(
+                        double.Parse(
+                            service["Interval"]))
+                    : TimeSpan.FromSeconds(20)
+            };
+        });
+        return Services;
     }
 }
