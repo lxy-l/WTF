205d8e1b18cc928f22a336dd4616a50c77471d73
Roy <lxysac@foxmail.com>
2022-12-13 13:41:45 +0800

    添加页面文件

--- src/Services/AuthService/AuthService/Program.cs
+++ src/Services/AuthService/AuthService/Program.cs
@@ -11,6 +11,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityServerConfig(connectionString);
