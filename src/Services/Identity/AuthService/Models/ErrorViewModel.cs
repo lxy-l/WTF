ca0c71543d4a18fdfa58457f9bf68b2aa0cfeb72
Roy <lxysac@foxmail.com>
2023-04-27 15:27:35 +0800

    分离AuthService项目，删除不必要的文件，删除dev分支

--- src/Services/Identity/AuthService/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AuthService.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- src/Services/Identity/AuthService/Models/UserViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace AuthService.Models
-{
-    public class UserViewModel:IdentityUser
-    {
-    }
-}
